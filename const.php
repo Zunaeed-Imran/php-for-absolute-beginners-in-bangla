f9a2ee43728ad70a1fb2c3d7a1c794bda2dee37d
Zunaeed-Imran <zunaeede@gmail.com>
2023-08-20 18:16:26 +0600

    main -const

--- const.php
+++ const.php
@@ -1,4 +1,8 @@
-// define('a', '960');
-// echo a;
-// define('car', ['toyota', 'bmw', 'ferari']);
-// echo car[0];
+<?php
+define('a', '960');
+echo a;
+define('car', ['toyota', 'bmw', 'ferari']);
+echo car[0];
+
+
+?>
