ab9523084c3002dda1d493eb24b48b9c6bf33e89
Zunaeed-Imran <zunaeede@gmail.com>
2023-08-21 18:11:23 +0600

    main -object class on PHP -practice object claass -42:45 in video

--- index.php
+++ index.php
@@ -47,4 +47,15 @@
 // }
 // test2('ruham');
 
+
+// class myNumber{
+//   function myPhone($num){
+//     echo "Hello this is $num <br>";
+//   }
+// }
+// $airBut = new myNumber();
+// $airBut->myPhone("Nothing Air1")
+
+
+
 ?>
