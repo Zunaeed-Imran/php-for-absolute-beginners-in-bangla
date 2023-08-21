8f3156bc2df074bbc4047eb19b455d22e9b906a2
Zunaeed-Imran <zunaeede@gmail.com>
2023-08-21 21:33:51 +0600

    main -rule add

--- 2_variable.php
+++ 2_variable.php
@@ -2,6 +2,7 @@
 $name = "imran";
 $age = 26;
 
-echo "this is {$name} i am {$age} ";
+echo "this is {$name} i am {$age} "; // if i $Name this it won't work
+                                    // becouse variable are case sensitive
 
 ?>
