6cbf04efe9fcb4f43ec7d1d511db40e636c31631
Zunaeed-Imran <zunaeede@gmail.com>
2023-08-21 11:40:53 +0600

    main -switch statement basic php

---
+++ switch.php
@@ -0,0 +1,12 @@
+<?php
+  switch('red'){
+  case 'red':
+    echo "Apple is red";
+    break;
+  case 'green':
+    echo "Banana is green and yellow";
+    break;
+  default : "I love fruit";    
+}
+
+?>
