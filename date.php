75dff0a997da9006a356bc52c0b320b67c6d5250
Zunaeed-Imran <zunaeede@gmail.com>
2023-08-21 15:44:15 +0600

    main -date in asian time -function time

--- date.php
+++ date.php
@@ -1,3 +1,7 @@
 <?php
-echo "today is ". date("d/m/y");
+// echo "today is ". date("d/m/y").'<br>';
+// echo "today time is ". date("h:i:sa"); // this is america default time zone
+
+date_default_timezone_set("Asia/Dhaka");
+echo "today time is ". date("h:i:sa");  // this is bangladesh time
 ?>
