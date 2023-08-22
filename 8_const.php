f37f026054f76c8dc3c8c791fdd57455baa18848
imran <zunaeede@gmail.com>
2023-08-22 15:35:15 +0600

    main -rule add

--- 8_const.php
+++ 8_const.php
@@ -2,8 +2,8 @@
 
 define('a', '960'); // define is a const function
 echo a."<br>"; 
-define('car', ['toyota', 'bmw', 'ferari']); 
-echo car[0];
+define('car', ['toyota', 'bmw', 'ferari']); // first const name than constant value
+echo car[0];                                // we can change the value but we should no change name
 
 
 ?>
