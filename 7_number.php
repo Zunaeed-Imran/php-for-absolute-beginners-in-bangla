7a990729f6c199f0c296d112e232732693fd20ef
imran <zunaeede@gmail.com>
2023-08-22 12:21:09 +0600

    main -number  function

---
+++ 7_number.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 5; // it integer called number
+$b = 10;  // it called float number
+
+echo max(45,6,9,2,5,8,58).'<br>'; // it will show the max number
+echo min(45,6,9,2,5,8,58).'<br>'; // it will show the minimum number
+echo abs(-58).'<br>'; // it will show the absolute number
+echo sqrt(16).'<br>'; // it will root the number.
+echo round(pi()).'<br>'; // it will round the number 3.1406 = 3
+echo rand(10, 20).'<br>'; // it will show random number between 10 - 20
+
+
+?>
