971106b14a8d441b02bdc8c1baff1d96bf3b2fb4
imran <zunaeede@gmail.com>
2023-08-26 13:05:10 +0600

    prac1 - object cleass

---
+++ prac4.php
@@ -0,0 +1,12 @@
+<?php
+ // object practice
+
+ class po{
+    function panda($m, $p, $s, $o){
+        echo "hy this is $o, $m, $p, $s";
+    }
+ }
+$kungfupanda = new po();
+$kungfupanda->panda("mantos", "panda", "sifu", "Ogway");
+ 
+?>
