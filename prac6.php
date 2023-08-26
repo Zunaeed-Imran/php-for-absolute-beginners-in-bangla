d0039b9887a483ca124296b8683f1231f57b46eb
imran <zunaeede@gmail.com>
2023-08-26 13:40:57 +0600

    prac1 -making calculator -not yet mak

---
+++ prac6.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="prac6.php" method="get">
+        <input type="number" name="num1" placeholder="Enter 1st number"><br>
+        <input type="number" name="num2" placeholder="Enter 2nd number"><br>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
+
+<?php
+
+class cal{
+    function all($a, $b){
+
+    }
+}
+
+?>
