cf8650dee0c00fb062a89af2f85c4d6e6cdfa323
imran <zunaeede@gmail.com>
2023-08-27 11:02:15 +0600

    prac1 -switch statement -practice

--- README.md
+++ README.md
@@ -32,3 +32,4 @@
 18. Function
 #### Practice some my own in prac1 branch
 06. prac6 // have practic a calculator in other branch "prac1" branch
+10. prac10 // have practice multipale case in switch statement "prac10" branch "prac10"
---
+++ prac10.php
@@ -0,0 +1,18 @@
+<?php
+
+switch('Apple'){
+    case "Banana":
+    case "Orange":
+    case "Apple":
+      echo "That fruit are good Vitamin";
+     break;
+    case "Cocunat":
+    case "Pinaple":
+    case "Bangi":
+      echo "Those fruits Hard to find";
+     break;
+    default:
+      echo "Tell me any fruits name";                      
+}
+
+?>
